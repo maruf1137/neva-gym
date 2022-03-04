de2902388fb4e3c634af8ad66a9dacd1554a1151
maruf <mmaruf1137@gmail.com>
2022-03-04 11:29:31 +0600

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+// show mobile nav
+
+const showMblNav = document.querySelector(".bars");
+const closeMblNav = document.querySelector(".btn-close");
+const nav = document.querySelector(".nav-container");
+// const navLink = document.querySelectorAll(".mobile-nav__link");
+
+const showNav = function () {
+  nav.classList.toggle("show-nav");
+};
+const closeNav = function () {
+  nav.classList.remove("show-nav");
+};
+
+showMblNav.addEventListener("click", showNav);
+closeMblNav.addEventListener("click", closeNav);
+
+// navLink.forEach((e) => {
+//   e.addEventListener("click", closeMblNav);
+// });
+
+// after before slider
+
+const slider1 = document.querySelector(".input-1");
+const img2 = document.querySelector(".experience__img--2");
+const dragLine1 = document.querySelector(".drag-line-1");
+
+const slider2 = document.querySelector(".input-2");
+const img4 = document.querySelector(".experience__img--4");
+const dragLine2 = document.querySelector(".drag-line-2");
+
+slider1.oninput = () => {
+  let sliderVal = slider1.value;
+  dragLine1.style.left = `${sliderVal}%`;
+  img2.style.width = sliderVal + "%";
+};
+
+slider2.oninput = () => {
+  let sliderVal = slider2.value;
+  dragLine2.style.left = `${sliderVal}%`;
+  img4.style.width = sliderVal + "%";
+};
