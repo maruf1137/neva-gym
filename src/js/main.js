e062bf1fae4f9a31314a4d0a50a46f6d9c73bd49
maruf <mmaruf1137@gmail.com>
2022-03-04 18:39:48 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -40,3 +40,63 @@ slider2.oninput = () => {
   dragLine2.style.left = `${sliderVal}%`;
   img4.style.width = sliderVal + "%";
 };
+
+// owl carousel
+
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  // dotContainer.addEventListener("click", function (e) {
+  //   if (e.target.classList.contains("dots__dot")) {
+  //     const { slide } = e.target.dataset;
+  //     goToSlide(slide);
+  //     activateDot(slide);
+  //   }
+  // });
+};
+slider();
